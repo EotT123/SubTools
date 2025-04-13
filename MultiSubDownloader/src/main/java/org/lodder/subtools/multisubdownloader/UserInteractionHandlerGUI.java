9bd8f3844de50af7d4c1c5653b626e3dd92f70e1
EotT123 <EotT123>
2025-04-13 19:42:43 +0200

    removed unused Getter annotation

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/UserInteractionHandlerGUI.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/UserInteractionHandlerGUI.java
@@ -3,13 +3,11 @@ package org.lodder.subtools.multisubdownloader;
 import javax.swing.*;
 import java.util.List;
 
-import lombok.Getter;
 import org.lodder.subtools.multisubdownloader.gui.dialog.SelectDialog;
 import org.lodder.subtools.sublibrary.data.UserInteractionSettingsIntf;
 import org.lodder.subtools.sublibrary.model.Release;
 import org.lodder.subtools.sublibrary.model.Subtitle;
 
-@Getter
 public class UserInteractionHandlerGUI extends org.lodder.subtools.sublibrary.userinteraction.UserInteractionHandlerGUI
     implements UserInteractionHandler {
 
