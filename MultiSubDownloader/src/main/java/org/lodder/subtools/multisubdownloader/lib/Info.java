f74e6623d3e8637b7fce14f735e6a5673dbc417f
EotT123 <EotT123>
2026-06-13 12:02:42 +0200

    remove duplicated loggings

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/Info.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/Info.java
@@ -1,5 +1,7 @@
 package org.lodder.subtools.multisubdownloader.lib;
 
+import java.util.function.Consumer;
+
 import org.jspecify.annotations.NullMarked;
 import org.lodder.subtools.multisubdownloader.settings.SettingsControl;
 import org.lodder.subtools.multisubdownloader.settings.model.Settings;
@@ -18,11 +20,7 @@ public class Info {
     }
 
     public static void subtitleSources(boolean isCli) {
-        if (isCli) {
-            System.out.println("----- Subtitle Providers ------");
-        } else {
-            LOGGER.info("----- Subtitle Providers ------");
-        }
+        display(isCli, "----- Subtitle Providers ------");
         Settings settings = SettingsControl.settings;
         for (SubtitleSource source : SubtitleSource.values()) {
             boolean enabled = switch (source) {
@@ -34,48 +32,28 @@ public class Info {
                 case SUBSCENE -> settings.serieSourceSubscene;
                 case SUBDL -> settings.serieSourceSubdl;
             };
-            if (isCli) {
-                System.out.println(" - provider : " + source + " enabled: " + enabled);
-            } else {
-                LOGGER.info(" - provider : " + source + " enabled: " + enabled);
-            }
+            display(isCli, " - provider : " + source + " enabled: " + enabled);
         }
-        if (isCli) {
-            System.out.println("-----------------------------");
-        } else {
-            LOGGER.info("-----------------------------");
-        }
+        display(isCli, "-----------------------------");
     }
 
     public static void subtitleFiltering(boolean isCli) {
         Settings settings = SettingsControl.settings;
-        if (isCli) {
-            System.out.println("----- Subtitle Filtering ------");
-            System.out.println(" - OptionSubtitleExactMatch : " + settings.optionSubtitleExactMatch);
-            System.out.println(" - OptionSubtitleKeywordMatch : " + settings.optionSubtitleKeywordMatch);
-            System.out.println(
-                    " - OptionSubtitleExcludeHearingImpaired : " + settings.optionSubtitleExcludeHearingImpaired);
-            System.out.println("-------------------------------");
-        } else {
-            LOGGER.info("----- Subtitle Filtering ------");
-            LOGGER.info(" - OptionSubtitleExactMatch: {} ", settings.optionSubtitleExactMatch);
-            LOGGER.info(" - OptionSubtitleKeywordMatch: {} ", settings.optionSubtitleKeywordMatch);
-            LOGGER.info(" - OptionSubtitleExcludeHearingImpaired: {} ", settings.optionSubtitleExcludeHearingImpaired);
-            LOGGER.info("-------------------------------");
-        }
-
+        display(isCli, "----- Subtitle Filtering ------");
+        display(isCli, " - OptionSubtitleExactMatch: " + settings.optionSubtitleExactMatch);
+        display(isCli, " - OptionSubtitleKeywordMatch: " + settings.optionSubtitleKeywordMatch);
+        display(isCli, " - OptionSubtitleExcludeHearingImpaired: " + settings.optionSubtitleExcludeHearingImpaired);
+        display(isCli, "-------------------------------");
     }
 
     public static void downloadOptions(boolean isCli) {
-        if (isCli) {
-            System.out.println("----- Download Options ------");
-            System.out.println(" - OptionsAlwaysConfirm : " + SettingsControl.settings.optionsAlwaysConfirm);
-            System.out.println("-----------------------------");
-        } else {
-            LOGGER.info("----- Download Options ------");
-            LOGGER.info(" - OptionsAlwaysConfirm : " + SettingsControl.settings.optionsAlwaysConfirm);
-            LOGGER.info("-----------------------------");
-        }
+        display(isCli, "----- Download Options ------");
+        display(isCli, " - OptionsAlwaysConfirm : " + SettingsControl.settings.optionsAlwaysConfirm);
+        display(isCli, "-----------------------------");
+    }
 
+    private static void display(boolean isCli, String value) {
+        Consumer<String> outputter = isCli ? System.out::println : LOGGER::info;
+        outputter.accept(value);
     }
 }
